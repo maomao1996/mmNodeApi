77bf276655515cf0859b668fe8f8215ff929d007
maomao1996 <1714487678@qq.com>
2018-08-24 15:50:43 +0800

    fix：优化路由注册类

--- util/route.js
+++ util/route.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
 const Router = require('koa-router');
 const { qq, netease, axios } = require('./axios/index.js');
 
-const fnQQ = fn => (ctx, next) => fn(ctx, next, qq, axios);
-const fnNetease = fn => (ctx, next) => fn(ctx, next, netease, axios);
+// 路由包装函数
+const func = (fn, type) => {
+    switch (type) {
+    case 'qq':
+        return (ctx, next) => fn(ctx, next, qq, axios);
+    case '163':
+        return (ctx, next) => fn(ctx, next, netease, axios);
+    }
+};
+
+/**
+ * 路由注册类
+ * 通过读取指定目录下的文件自动注册路由
+ */
 
 module.exports = class Route {
     constructor (name) {
@@ -11,8 +24,13 @@ module.exports = class Route {
     }
     init () {
         const path = this.name.replace(/_/g, '/');
-        this.router.get('/qq', fnQQ(require(`../routes/${this.name}/qq.js`)));
-        this.router.get('/163', fnNetease(require(`../routes/${this.name}/163.js`)));
+        fs
+            .readdirSync(`./routes/${this.name}/`)
+            .reverse()
+            .forEach(file => {
+                const fileName = file.replace(/.js/, '');
+                this.router.get(`/${fileName}`, func(require(`../routes/${this.name}/${file}`), fileName));
+            });
         this.router.redirect(`/`, `/${path}/qq`);
         return this.router;
     }
