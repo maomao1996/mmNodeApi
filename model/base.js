2f829c54332f66258cff0818c1f0fca6204955a0
maomao1996 <1714487678@qq.com>
2018-09-30 16:38:17 +0800

    feat：增加 创建者信息 基础类

--- model/base.js
+++ model/base.js
@@ -2,6 +2,7 @@
  * 基础类
  */
 
+// 歌曲信息
 class BaseSong {
     constructor ({ name, singer }) {
         this.name = name; // 歌曲名
@@ -9,6 +10,16 @@ class BaseSong {
     }
 }
 
+// 创建者信息
+class Creator {
+    constructor ({ uid, name, picUrl }) {
+        this.uid = uid; // uid
+        this.name = name; // 名称
+        this.picUrl = picUrl; // 头像
+    }
+}
+
 module.exports = {
-    BaseSong
+    BaseSong,
+    Creator
 };
