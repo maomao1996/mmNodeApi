eade43b0127529c8492e361b2910d734f17f9bbc
maomao1996 <1714487678@qq.com>
2018-10-15 17:53:57 +0800

    feat：增加 post 请求处理和参数校验

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const Koa = require('koa');
 const koaStatic = require('koa-static');
+const KoaBody = require('koa-body');
 const path = require('path');
 const cors = require('koa2-cors');
 const config = require('./config/index.js');
 const router = require('./routes/index.js');
+const Tips = require('./util/tips.js');
 
 const app = new Koa();
 
@@ -12,6 +14,20 @@ app.use(cors({ origin: '*' }));
 
 // 托管静态文件
 app.use(koaStatic(path.resolve(__dirname, 'static')));
+
+// 处理 post 请求
+app.use(KoaBody());
+
+// 参数校验
+app.use(async (ctx, next) => {
+    if (/(detail|lyric)/.test(ctx.url)) {
+        if (!ctx.query.id) {
+            ctx.body = Tips[1001];
+            return;
+        }
+    }
+    await next();
+});
 
 // 调用路由中间件
 app.use(router.routes());
