4c7eb08d4cfd5439ec7682e3a8ca9b660dcfd245
maomao1996 <1714487678@qq.com>
2019-10-25 17:10:02 +0800

    feat：增加 网易云用户歌单接口

---
+++ routes/user_playlist/163.js
@@ -0,0 +1,33 @@
+const { formatPlayList } = require('../../model/index.js');
+const config = require('../../config/index.js');
+const { Tips, OK_163 } = require('../../util/index.js');
+
+// 歌曲 URL 网易
+
+module.exports = async (ctx, next, axios) => {
+    const ft = ctx.query.format || config.format;
+    const { uid } = ctx.query;
+    const offset = parseInt(ctx.query.offset || 0);
+    const limit = parseInt(ctx.query.limit || 1000);
+    const params = {
+        uid: uid,
+        limit,
+        offset
+    };
+    await axios('/weapi/user/playlist', 'post', params)
+        .then(res => {
+            if (res.code === OK_163) {
+                const data =
+                    ft === 'open'
+                        ? formatPlayList(res.playlist, '163')
+                        : res.playlist;
+                ctx.body = {
+                    data,
+                    ...Tips[163]
+                };
+            } else {
+                ctx.body = res;
+            }
+        })
+        .catch(() => ctx.throw(500));
+};
